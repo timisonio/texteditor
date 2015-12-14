d9bd3f07f77ce68c41269e380f7046d0c95b5fd2
Tim Isonio <timisonio@ucla.edu>
2015-12-14 00:12:12 -0800

    open files

--- Line.h
+++ Line.h
@@ -4,17 +4,20 @@
 #include <string>
 
 class Line{
- private:
+private:
   std::string str1;
   std::string str2;
-  
+
 public:
+  Line() {};
+  Line(std::string contents) { str1 = contents; }
   bool insert(std::string input);
   bool pop_back();
   bool left();
   bool right();
   std::string get_contents() { return str1 + str2; }
   int get_cursor_position() { return str1.length(); }
+  int get_length() { return str1.length() + str2.length(); }
 };
 
 #endif
